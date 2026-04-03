ab465f31b60d775ece0dafa8561bd4c91bd27e24
黄绍基 <@huangshoaji>
2026-04-03 17:50:08 +0800

    style: format localized statistics indicators

--- kafka-ui-react-app/src/components/Topics/Topic/Statistics/Indicators/SizeStats.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Statistics/Indicators/SizeStats.tsx
@@ -4,6 +4,52 @@ import { TopicAnalysisSizeStats } from 'generated-sources';
 import BytesFormatted from 'components/common/BytesFormatted/BytesFormatted';
 import { useTranslation } from 'components/contexts/LocaleContext';
 
+const TranslatedIndicators: React.FC<TopicAnalysisSizeStats> = ({
+  sum,
+  min,
+  max,
+  avg,
+  prctl50,
+  prctl75,
+  prctl95,
+  prctl99,
+  prctl999,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Metrics.Indicator label={t('topics.statistics.size.totalSize')}>
+        <BytesFormatted value={sum} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.minSize')}>
+        <BytesFormatted value={min} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.maxSize')}>
+        <BytesFormatted value={max} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.average')}>
+        <BytesFormatted value={avg} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.percentile50')}>
+        <BytesFormatted value={prctl50} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.percentile75')}>
+        <BytesFormatted value={prctl75} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.percentile95')}>
+        <BytesFormatted value={prctl95} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.percentile99')}>
+        <BytesFormatted value={prctl99} />
+      </Metrics.Indicator>
+      <Metrics.Indicator label={t('topics.statistics.size.percentile999')}>
+        <BytesFormatted value={prctl999} />
+      </Metrics.Indicator>
+    </>
+  );
+};
+
 const SizeStats: React.FC<{
   stats: TopicAnalysisSizeStats;
   title: string;
@@ -25,51 +71,5 @@ const SizeStats: React.FC<{
     />
   </Metrics.Section>
 );
-
-const TranslatedIndicators: React.FC<TopicAnalysisSizeStats> = ({
-  sum,
-  min,
-  max,
-  avg,
-  prctl50,
-  prctl75,
-  prctl95,
-  prctl99,
-  prctl999,
-}) => {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <Metrics.Indicator label={t('topics.statistics.size.totalSize')}>
-      <BytesFormatted value={sum} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.minSize')}>
-      <BytesFormatted value={min} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.maxSize')}>
-      <BytesFormatted value={max} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.average')}>
-      <BytesFormatted value={avg} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.percentile50')}>
-      <BytesFormatted value={prctl50} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.percentile75')}>
-      <BytesFormatted value={prctl75} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.percentile95')}>
-      <BytesFormatted value={prctl95} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.percentile99')}>
-      <BytesFormatted value={prctl99} />
-      </Metrics.Indicator>
-      <Metrics.Indicator label={t('topics.statistics.size.percentile999')}>
-      <BytesFormatted value={prctl999} />
-      </Metrics.Indicator>
-    </>
-  );
-};
 
 export default SizeStats;
